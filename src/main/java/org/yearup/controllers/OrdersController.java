b12d8305412299099ee6f780038fd4c91407bb50
linotmike <128604219+linotmike@users.noreply.github.com>
2024-12-16 17:52:48 -0800

    Made a model,DAO and interface class for orders to checkout orders

---
+++ src/main/java/org/yearup/controllers/OrdersController.java
@@ -0,0 +1,13 @@
+package org.yearup.controllers;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@CrossOrigin(origins = "*")
+@RequestMapping("/orders")
+public class OrdersController {
+
+
+}
---
+++ src/main/java/org/yearup/data/mysql/MySqlOrderDao.java
@@ -0,0 +1,14 @@
+package org.yearup.data.mysql;
+
+import org.springframework.stereotype.Component;
+import org.yearup.data.OrderDao;
+
+import javax.sql.DataSource;
+
+@Component
+public class MySqlOrderDao extends MySqlDaoBase implements OrderDao {
+
+    public MySqlOrderDao(DataSource dataSource) {
+        super(dataSource);
+    }
+}
